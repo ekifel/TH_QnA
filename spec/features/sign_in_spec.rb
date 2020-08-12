04ad0f684b25e2c974fec6a893f4848ef782bfd8
ekifel <kifel.ernest@gmail.com>
2020-08-12 20:01:21 +0500

    refactor sign in features tests

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -6,19 +6,18 @@ feature 'User can sign in', %q{
   I'd like to be able to sign in
 } do
 
-  scenario 'Registered user tries to sign in' do
-    User.create!(email: 'user@test.com', password: '12345678')
+  given(:user) { User.create!(email: 'user@test.com', password: '12345678') }
+  background { visit new_user_session_path }
 
-    visit new_user_session_path
-    fill_in 'Email', with: 'user@test.com'
-    fill_in 'Password', with: '12345678'
+  scenario 'Registered user tries to sign in' do
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
     click_on 'Log in'
 
     expect(page).to have_content 'Signed in successfully.'
   end
 
   scenario 'Unregistered user tries to sign in' do
-    visit new_user_session_path
     fill_in 'Email', with: 'wrong@test.com'
     fill_in 'Password', with: '12345678'
     click_on 'Log in'
