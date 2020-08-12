8b950711793004cff4558a106d0d07b3838ccdd8
ekifel <kifel.ernest@gmail.com>
2020-08-12 19:58:51 +0500

    fix reg user sign in  scenario & add unreg user scenario

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -12,11 +12,17 @@ feature 'User can sign in', %q{
     visit new_user_session_path
     fill_in 'Email', with: 'user@test.com'
     fill_in 'Password', with: '12345678'
+    click_on 'Log in'
 
     expect(page).to have_content 'Signed in successfully.'
   end
 
   scenario 'Unregistered user tries to sign in' do
+    visit new_user_session_path
+    fill_in 'Email', with: 'wrong@test.com'
+    fill_in 'Password', with: '12345678'
+    click_on 'Log in'
 
+    expect(page).to have_content 'Invalid Email or password'
   end
 end
