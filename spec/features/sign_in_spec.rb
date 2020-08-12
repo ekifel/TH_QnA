83032b75274b24725161352e80f3794e12d0669a
ekifel <kifel.ernest@gmail.com>
2020-08-12 19:35:46 +0500

    add_scenario_to_registered_user

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'User can sign in', %q{
+  In order to ask questions
+  As an unauthenticated user
+  I'd like to be able to sign in
+} do
+
+  scenario 'Registered user tries to sign in' do
+    User.create!(email: 'user@test.com', password: '1234')
+
+    visit '/login'
+    fill_in 'Email', with: 'user@test.com'
+    fill_in 'Password', with: '1234'
+
+    expect(page).to have_content 'Signed in successfully.'
+  end
+
+  scenario 'Unregistered user tries to sign in' do
+    
+  end
+end
