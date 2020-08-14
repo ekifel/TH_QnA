e43216c6f572ebc138d2b3a08c8c3d4864f46288
ekifel <kifel.ernest@gmail.com>
2020-08-14 18:12:35 +0500

    add references between user/answer/question models

---
+++ db/migrate/20200814130900_add_user_reference_to_answers_and_questions.rb
@@ -0,0 +1,6 @@
+class AddUserReferenceToAnswersAndQuestions < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :answers, :user, foreign_key: true
+    add_reference :questions, :user, foreign_key: true
+  end
+end
